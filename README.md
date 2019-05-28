e9880000ff013eb21ec3dda55c7577e5f56c3ad0
fthmarasli <fthmarasli@gmail.com>
2019-05-28 13:01:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-b7qum6
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-b7qum6)
---
+++ index.js
@@ -0,0 +1,27 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+const appDiv = document.getElementById('app');
+appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+const {google} = require('googleapis');
+function getAccessToken() {
+  return new Promise(function(resolve, reject) {
+    var key = require('./service-account.json');
+    var jwtClient = new google.auth.JWT(
+      key.client_email,
+      null,
+      key.private_key,
+      SCOPES,
+      null
+    );
+    jwtClient.authorize(function(err, tokens) {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(tokens.access_token);
+    });
+  });
+}
